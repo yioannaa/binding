07a9c3c262a732b4f8ab1b50e00aa270a48a1584
Joanna Krzyzanowska <joanna.krzyzanowska@o2.pl>
2022-09-05 06:12:59 +0000

    up to 4 step

---
+++ webapp/index.js
@@ -0,0 +1,29 @@
+sap.ui.require([
+	"sap/m/Text",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/mvc/XMLView"
+], function (Text, JSONModel, XMLView) {
+	"use strict";
+
+	// Attach an anonymous function to the SAPUI5 'init' event
+	sap.ui.getCore().attachInit(function () {
+
+        		// Create a JSON model from an object literal
+		var oModel = new JSONModel({
+			firstName: "Harry",
+            lastName: "Hawk",
+            enabled: true,
+            panelHeaderText: "Data Binding Basics"
+		});
+
+		// Assign the model object to the SAPUI5 core
+		sap.ui.getCore().setModel(oModel);
+
+        new XMLView({
+            viewName:"sap.ui.demo.db.view.App"
+        }).placeAt("content");
+
+
+	
+	});
+});
